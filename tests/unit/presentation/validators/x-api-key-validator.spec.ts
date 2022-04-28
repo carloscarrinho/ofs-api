b65694068333d04879267a6e919009ebe6d826c8
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-04-28 20:37:09 -0300

    test: ensure XApiKeyValidator returns InvalidParamError if x-api-key is not valid

---
+++ tests/unit/presentation/validators/x-api-key-validator.spec.ts
@@ -0,0 +1,19 @@
+import { InvalidParamError } from "../../../../src/presentation/errors/invalid-param-error";
+import { XApiKeyValidator } from "../../../../src/presentation/validators/x-api-key-validator";
+
+describe("Unit", () => {
+  describe("Presentation::Validators", () => {
+    describe("XApiKeyValidator.validate()", () => {
+      it("Should return InvalidParamError if x-api-key is not valid", async () => {
+        // GIVEN
+        const validator = new XApiKeyValidator();
+
+        // WHEN
+        const error = validator.validate("not-valid");
+
+        // THEN
+        expect(error).toEqual(new InvalidParamError("x-api-key"));
+      });
+    });
+  });
+});
