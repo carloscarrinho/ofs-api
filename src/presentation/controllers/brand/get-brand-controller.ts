bddd2319cfccff43c0f1a52e2bb61369f7ba2e37
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-04-29 15:07:36 -0300

    feat: ensure GetBrandController returns 400 if brand id is missing on request header

--- src/presentation/controllers/brand/get-brand-controller.ts
+++ src/presentation/controllers/brand/get-brand-controller.ts
@@ -1,11 +1,20 @@
-import { ok } from "../../helpers/http-helper";
+import { MissingParamError } from "../../errors/missing-param-error";
+import { badRequest, ok, serverError } from "../../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
+import { IValidation } from "../../validators/ivalidation";
 import { IController } from "../icontroller";
 
 export class GetBrandController implements IController {
+  constructor(private readonly validator: IValidation) {}
+  
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    return ok({
-      success: "ok",
-    });
+    const error = this.validator.validate(request.params);
+    if(error) return badRequest(error);
+
+    try {
+      return ok({ success: request.params.brandId });
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
--- tests/unit/presentation/controllers/get-brand-controller.spec.ts
+++ tests/unit/presentation/controllers/get-brand-controller.spec.ts
@@ -1,9 +1,18 @@
 import { GetBrandController } from "../../../../src/presentation/controllers/brand/get-brand-controller";
 import { IController } from "../../../../src/presentation/controllers/icontroller";
 import { HttpRequest } from "../../../../src/presentation/protocols/http";
+import { IValidation } from "../../../../src/presentation/validators/ivalidation";
 
-const makeController = (): IController => {
-  return new GetBrandController();
+const makeController = ({
+  validate
+}:{
+  validate?: Function
+}): IController => {
+  const validation = {
+    validate: validate ?? jest.fn().mockReturnValueOnce(null),
+  } as unknown as IValidation;
+
+  return new GetBrandController(validation);
 }
 
 const makeRequest = (data?: object): HttpRequest => ({
@@ -14,10 +23,28 @@ const makeRequest = (data?: object): HttpRequest => ({
 describe('Unit', () => {
   describe('Presentation::Controllers', () => {
     describe('GetBrandController.handle()', () => {
+      it('Should return 400 if params Validation fails', async () => {
+        // GIVEN
+        const brandController = makeController({
+          validate: jest.fn().mockReturnValueOnce(new Error())
+        });
+        const request = makeRequest({
+          params: { brandId: "some-id" }
+        });
+
+        // WHEN
+        const response = await brandController.handle(request);
+
+        // THEN
+        expect(response.statusCode).toStrictEqual(400);
+      });
+      
       it('Should return 200 if everything is OK', async () => {
         // GIVEN
-        const brandController = makeController();
-        const request = makeRequest();
+        const brandController = makeController({});
+        const request = makeRequest({
+          params: { brandId: "some-id" }
+        });
 
         // WHEN
         const response = await brandController.handle(request);
