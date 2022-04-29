31c7da539c1688142cb3d76108be932aacdb0586
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-04-29 12:23:01 -0300

    test: ensure DynamoBrandRepository stores the data successfully into DynamoDB

--- tests/integration/infrastructure/db/repositories/brand/dynamo-brand-repository.spec.ts
+++ tests/integration/infrastructure/db/repositories/brand/dynamo-brand-repository.spec.ts
@@ -1,4 +1,6 @@
 import { DynamoDB } from "aws-sdk";
+import { } from "aws-sdk/";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { DynamoBrandRepository } from "../../../../../../src/infrastructure/db/repositories/brand/dynamo-brand-repository";
 import { IBrandRepository } from "../../../../../../src/infrastructure/db/repositories/brand/ibrand-repository";
 
@@ -32,7 +34,9 @@ const prepareEnvironment = async () => {
 const teardownEnvironment = async () => {
   try {
     const tables = await dynamoDb.listTables().promise();
-    const foundTable = tables.TableNames?.some((name: string) => [process.env.TABLE_NAME].includes(name));
+    const foundTable = tables.TableNames?.some((name: string) =>
+      [process.env.TABLE_NAME].includes(name)
+    );
 
     if (!foundTable) return;
 
@@ -46,24 +50,25 @@ const makeDynamoBrandRepository = (): IBrandRepository => {
   const settings = {
     tableEndpoint: process.env.TABLE_ENDPOINT,
     tableName: process.env.TABLE_NAME,
-  }
-  
+  };
+
   return new DynamoBrandRepository(settings);
 };
 
 const defaultNewBrandData = { id: "some-id", name: "any_name" };
 
-
 describe("Integration", () => {
   describe("Infastructure::DB::Repositories::Brand", () => {
     beforeEach(prepareEnvironment);
     afterEach(teardownEnvironment);
-    
-    describe("DynamoBrandRepository.store()", () => {  
+
+    describe("DynamoBrandRepository.store()", () => {
       it("Should throw an error if DynamoDB throws", async () => {
         // Given
         // forcing an error because the table does not exists.
-        await dynamoDb.deleteTable({ TableName: process.env.TABLE_NAME }).promise();
+        await dynamoDb
+          .deleteTable({ TableName: process.env.TABLE_NAME })
+          .promise();
         const brandRepository = makeDynamoBrandRepository();
 
         // When
@@ -71,6 +76,27 @@ describe("Integration", () => {
 
         // Then
         await expect(result).rejects.toThrow();
+      });
+
+      it("Should return the brand data if brand was stored successfully", async () => {
+        // Given
+        const dynamoClient = new DocumentClient({
+          endpoint: process.env.TABLE_ENDPOINT,
+        });
+        const brandRepository = makeDynamoBrandRepository();
+
+        // When
+        const result = await brandRepository.store(defaultNewBrandData);
+        const record = await dynamoClient
+          .get({ 
+            TableName: process.env.TABLE_NAME,
+            Key: { pk: `brand#${defaultNewBrandData.id}` } 
+          })
+          .promise();
+
+        // Then
+        expect(record.Item.name).toEqual(defaultNewBrandData.name);
+        expect(result).toStrictEqual(defaultNewBrandData);
       });
     });
   });
