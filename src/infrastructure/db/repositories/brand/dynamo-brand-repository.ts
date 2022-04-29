e8f4ce45247ceb9905547f3b9535d3f3a33aaa34
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-04-29 11:38:47 -0300

    feat: ensure DynamoBrandRepository throws an error if DynamoDB throws

---
+++ src/infrastructure/db/repositories/brand/dynamo-brand-repository.ts
@@ -0,0 +1,35 @@
+import { v4 as uuid } from "uuid";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { IBrandModel } from "../../../../application/use-cases/brand/iadd-brand";
+import { IBrand } from "../../../../domain/entities/ibrand";
+import { IBrandRepository } from "./ibrand-repository";
+
+interface DynamoSettings {
+  tableEndpoint: string;
+  tableName: string;
+}
+
+export class DynamoBrandRepository implements IBrandRepository {
+  private readonly client: DocumentClient;
+
+  constructor(private readonly settings: DynamoSettings) {
+    this.client = new DocumentClient({ endpoint: this.settings.tableEndpoint });
+  }
+
+  async store(brandData: IBrandModel): Promise<IBrand> {
+    const newBrandData = { id: uuid(), ...brandData }
+    
+    await this.client
+      .put({
+        TableName: this.settings.tableName,
+        Item: {
+          ...newBrandData,
+          pk: `brand#${newBrandData.id}`,
+          createdAt: new Date().toISOString(),
+        },
+      })
+      .promise();
+
+    return newBrandData;
+  }
+}
