ddaa2d385d24a06d3736892435a6fb0c450972fe
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-04-28 21:36:48 -0300

    refactor: ensure AddBrandController validates fields on body and removes x-api-key from validation

--- src/presentation/controllers/brand/add-brand-controller.ts
+++ src/presentation/controllers/brand/add-brand-controller.ts
@@ -1,16 +1,15 @@
-import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { IController } from "../icontroller";
-import { badRequest, forbbiden, ok } from "../../helpers/http-helper";
-import { MissingParamError } from "../../errors/missing-param-error";
-import { InvalidParamError } from "../../errors/invalid-param-error";
+import { HttpRequest, HttpResponse } from "../../protocols/http";
+import { badRequest, ok } from "../../helpers/http-helper";
+import { IValidation } from "../../validators/ivalidation";
 
 export class AddBrandController implements IController {
+  constructor(private validation: IValidation) {}
+  
   async handle(request: HttpRequest): Promise<HttpResponse> {
-    const missinXApiKey = request.header["x-api-key"] ? null : new MissingParamError("x-api-key");
-    const invalidXApiKey = request.header["x-api-key"] == "valid-api-key" ? null : new InvalidParamError("x-api-key");
+    const error = this.validation.validate(request.body);
     
-    if(missinXApiKey) return badRequest(missinXApiKey); 
-    if(invalidXApiKey) return forbbiden(invalidXApiKey); 
+    if(error) return badRequest(error); 
     
     return ok({ message: "ok" });
   }
--- tests/unit/presentation/controllers/add-brand-controller.spec.ts
+++ tests/unit/presentation/controllers/add-brand-controller.spec.ts
@@ -1,9 +1,15 @@
 import { AddBrandController } from "../../../../src/presentation/controllers/brand/add-brand-controller";
-import { IController } from "../../../../src/presentation/controllers/icontroller";
 import { HttpRequest } from "../../../../src/presentation/protocols/http";
+import { IValidation } from "../../../../src/presentation/validators/ivalidation";
+import { RequiredFieldsValidator } from "../../../../src/presentation/validators/required-fiels-validator";
+import { ValidationComposite } from "../../../../src/presentation/validators/validation-composite";
 
-const makeController = (): IController => {
-  return new AddBrandController();
+const makeController = (): AddBrandController => {
+  const validators: IValidation[] = [new RequiredFieldsValidator(["name"])];
+
+  const validation = new ValidationComposite(validators);
+
+  return new AddBrandController(validation);
 };
 
 const makeRequest = (data?: object): HttpRequest => ({
@@ -11,36 +17,25 @@ const makeRequest = (data?: object): HttpRequest => ({
     "Content-Type": "application/json",
     "x-api-key": "valid-api-key",
   },
+  body: {
+    name: "any-name",
+  },
   ...data,
 });
 
 describe("Unit", () => {
   describe("Presentation::Controllers", () => {
     describe("AddBrandController.handle()", () => {
-      it("Should return 400 if API_KEY is missing", async () => {
+      it("Should return 400 if validation returns error", async () => {
         // GIVEN
         const brandController = makeController();
-        const request = makeRequest({ header: {} });
+        const request = makeRequest({ header: {}, body: {} });
 
         // WHEN
         const response = await brandController.handle(request);
 
         // THEN
         expect(response.statusCode).toStrictEqual(400);
-      });
-
-      it("Should return 403 if API_KEY is not valid", async () => {
-        // GIVEN
-        const brandController = makeController();
-        const request = makeRequest({
-          header: { "x-api-key": "not-valid-api-key" },
-        });
-
-        // WHEN
-        const response = await brandController.handle(request);
-
-        // THEN
-        expect(response.statusCode).toStrictEqual(403);
       });
 
       it("Should return 200 if everything is OK", async () => {
