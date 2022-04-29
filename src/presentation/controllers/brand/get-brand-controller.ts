84b72831a1d0785bbe5dffc1d01c8a2b1200ff1f
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-04-29 15:42:19 -0300

    feat: ensure GetBrandController calls GetBrand with brand id from request header

--- src/presentation/controllers/brand/get-brand-controller.ts
+++ src/presentation/controllers/brand/get-brand-controller.ts
@@ -1,15 +1,20 @@
-import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest, ok, serverError } from "../../helpers/http-helper";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 import { IValidation } from "../../validators/ivalidation";
 import { IController } from "../icontroller";
+import { IGetBrand } from "../../../application/use-cases/brand/iget-brand";
 
 export class GetBrandController implements IController {
-  constructor(private readonly validator: IValidation) {}
+  constructor(
+    private readonly validator: IValidation,
+    private readonly getBrand: IGetBrand
+  ) {}
   
   async handle(request: HttpRequest): Promise<HttpResponse> {
     const error = this.validator.validate(request.params);
     if(error) return badRequest(error);
+
+    await this.getBrand.get(request.params?.brandId);
 
     try {
       return ok({ success: request.params.brandId });
