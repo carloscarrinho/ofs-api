e68cb401a5ae978d7a66471c865be64cf8732cb5
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-05-02 00:15:52 -0300

    feat: add add-offer handler

--- src/presentation/handler.ts
+++ src/presentation/handler.ts
@@ -1,6 +1,10 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { apiGatewayAdapter } from "../main/adapters/api-gateway-adapter";
-import { addBrandControllerFactory, getBrandControllerFactory } from "../main/factories";
+import {
+  addBrandControllerFactory,
+  getBrandControllerFactory,
+  addOfferControllerFactory,
+} from "../main/factories";
 
 export const getBrand = async (event: APIGatewayEvent) => {
   const response = await apiGatewayAdapter(event, getBrandControllerFactory());
@@ -13,6 +17,15 @@ export const getBrand = async (event: APIGatewayEvent) => {
 
 export const addBrand = async (event: APIGatewayEvent) => {
   const response = await apiGatewayAdapter(event, addBrandControllerFactory());
+
+  return {
+    statusCode: response.statusCode,
+    body: JSON.stringify(response.body),
+  };
+};
+
+export const addOffer = async (event: APIGatewayEvent) => {
+  const response = await apiGatewayAdapter(event, addOfferControllerFactory());
 
   return {
     statusCode: response.statusCode,
