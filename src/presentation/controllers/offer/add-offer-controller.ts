f4843134afeeb65e4643380c7a5c4248a2934bdf
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-05-01 22:18:55 -0300

    feat: ensure AddOfferController handles GetBrand calls before trying to add offer into dynamodb

--- src/presentation/controllers/offer/add-offer-controller.ts
+++ src/presentation/controllers/offer/add-offer-controller.ts
@@ -1,21 +1,31 @@
 import { IController } from "../icontroller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
-import { badRequest, ok, serverError } from "../../helpers/http-helper";
+import { badRequest, notFound, ok, serverError } from "../../helpers/http-helper";
 import { IValidation } from "../../validators/ivalidation";
 import { IAddOffer } from "../../../application/use-cases/offer/iadd-offer";
+import { IGetBrand } from "../../../application/use-cases/brand/iget-brand";
 
 export class AddOfferController implements IController {
   constructor(
     private readonly validation: IValidation,
+    private readonly getBrand: IGetBrand,
     private readonly addOffer: IAddOffer,
   ) {}
   
   async handle(request: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(request.body); 
     if(error) return badRequest(error); 
+    
+    const { brandId } = request.body;
 
     try {
-      await this.addOffer.add(request.body);
+      
+      const brand = await this.getBrand.get(brandId);
+      if (!brand) return notFound(brandId);
+
+      const offer = await this.addOffer.add(request.body);
+
+      return ok({ offer });
     } catch (err) {
       return serverError(err);
     }
--- tests/unit/presentation/controllers/offer/add-offer-controller.spec.ts
+++ tests/unit/presentation/controllers/offer/add-offer-controller.spec.ts
@@ -2,6 +2,7 @@ import { AddOfferController } from "../../../../../src/presentation/controllers/
 import { IValidation } from "../../../../../src/presentation/validators/ivalidation";
 import { HttpRequest } from "../../../../../src/presentation/protocols/http";
 import { IAddOffer } from "../../../../../src/application/use-cases/offer/iadd-offer";
+import { IGetBrand } from "../../../../../src/application/use-cases/brand/iget-brand";
 
 const defaultOfferModel = {
   brandId: "any-brand-id",
@@ -16,14 +17,24 @@ const defaultOfferModel = {
 
 const makeController = ({
   validate,
+  get,
   add,
 }: {
   validate?: Function;
+  get?: Function;
   add?: Function;
 }): AddOfferController => {
   const validation = {
     validate: validate ?? jest.fn().mockReturnValueOnce(null),
   } as unknown as IValidation;
+
+  const getBrand = {
+    get: get ?? jest.fn().mockReturnValueOnce({ 
+      id: "some-id", 
+      name: "any-name",
+      createdAt: "2022-04-29T10:00:00"
+    }),
+  } as unknown as IGetBrand;
 
   const addOffer = {
     add:
@@ -35,7 +46,7 @@ const makeController = ({
      }),
   } as unknown as IAddOffer;
   
-  return new AddOfferController(validation, addOffer);
+  return new AddOfferController(validation, getBrand, addOffer);
 };
 
 const makeRequest = (data?: object): HttpRequest => ({
@@ -77,6 +88,59 @@ describe("Unit", () => {
         expect(response.statusCode).toStrictEqual(400);
       });
 
+      it("Should call GetBrand with brand id", async () => {
+        // GIVEN
+        const dependencies = { get: jest.fn(), add: jest.fn() };
+        const addOfferController = makeController(dependencies);
+        const request = makeRequest();
+
+        // WHEN
+        addOfferController.handle(request);
+
+        // THEN
+        expect(dependencies.get).toBeCalledWith(request.body.brandId);
+        expect(dependencies.add).not.toBeCalled();
+      });
+
+      it("Should return 404 if brand id is invalid", async () => {
+        // Given
+        const dependencies = {
+          get: jest.fn().mockResolvedValueOnce(null),
+          add: jest.fn(),
+        };
+        const request = makeRequest();
+        const addOfferController = makeController(dependencies);
+
+        // When
+        const response = await addOfferController.handle(request);
+
+        // Then
+        expect(response.statusCode).toEqual(404);
+        expect(dependencies.add).not.toBeCalled();
+      });
+
+      it("Should return 500 if GetBrand throws an error", async () => {
+        // GIVEN
+        const error = new Error();
+        error.stack = "any_stack";
+
+        const dependencies = {
+          get: jest.fn().mockImplementationOnce(() => {
+            throw error;
+          }),
+          add: jest.fn(),
+        };
+        const addController = makeController(dependencies);
+        const request = makeRequest();
+
+        // WHEN
+        const response = await addController.handle(request);
+
+        // THEN
+        expect(response.statusCode).toStrictEqual(500);
+        expect(dependencies.add).not.toBeCalled();
+      });
+
       it("Should call AddOffer with required values", async () => {
         // GIVEN
         const dependencies = { add: jest.fn() };
@@ -84,7 +148,7 @@ describe("Unit", () => {
         const request = makeRequest();
 
         // WHEN
-        addOfferController.handle(request);
+        await addOfferController.handle(request);
 
         // THEN
         expect(dependencies.add).toBeCalledWith(request.body);
