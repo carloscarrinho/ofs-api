f722e10a26b0b255a42805be8649689b3f7a0d66
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-05-03 20:47:44 -0300

    feat: ensure DbLocation handles get location use case

--- src/application/use-cases/location/db-location.ts
+++ src/application/use-cases/location/db-location.ts
@@ -1,11 +1,16 @@
 import { ILocation, ILocationModel } from "../../../domain/entities/ilocation";
 import { ILocationRepository } from "../../../infrastructure/db/repositories/location/ilocation-repository";
 import { IAddLocation,  } from "./iadd-location";
+import { IGetLocation } from "./iget-location";
 
-export class DbLocation implements IAddLocation {
+export class DbLocation implements IAddLocation, IGetLocation {
   constructor(private readonly repository: ILocationRepository) {}
 
   async add(locationModel: ILocationModel): Promise<ILocation> {
     return await this.repository.store(locationModel);
+  }
+
+  async get(brandId: string, locationId: string): Promise<ILocation> {
+    return await this.repository.find(brandId, locationId);
   }
 }
--- tests/unit/application/use-cases/location/db-location.spec.ts
+++ tests/unit/application/use-cases/location/db-location.spec.ts
@@ -1,15 +1,22 @@
 import { DbLocation } from "../../../../../src/application/use-cases/location/db-location";
 import { ILocationRepository } from "../../../../../src/infrastructure/db/repositories/location/ilocation-repository";
-import { generateLocationModel, generateLocationEntity } from "../../../../fixtures/location/location-fixture";
+import {
+  generateLocationModel,
+  generateLocationEntity,
+} from "../../../../fixtures/location/location-fixture";
+
+const locationEntity = generateLocationEntity();
 
 const makeDbLocation = ({
   store,
+  find,
 }: {
   store?: Function;
   find?: Function;
 }): DbLocation => {
   const locationRepository = {
-    store: store ?? jest.fn().mockResolvedValueOnce(generateLocationEntity()),
+    store: store ?? jest.fn().mockResolvedValueOnce(locationEntity),
+    find: find ?? jest.fn().mockResolvedValueOnce(locationEntity),
   } as unknown as ILocationRepository;
 
   return new DbLocation(locationRepository);
@@ -21,10 +28,10 @@ describe("Unit", () => {
       it("Should call LocationRepository with required data", async () => {
         // GIVEN
         const dependencies = { store: jest.fn() };
-        const DbLocation = makeDbLocation(dependencies);
+        const dbLocation = makeDbLocation(dependencies);
 
         // WHEN
-        await DbLocation.add(generateLocationModel());
+        await dbLocation.add(generateLocationModel());
 
         // THEN
         expect(dependencies.store).toBeCalledWith(generateLocationModel());
@@ -37,10 +44,10 @@ describe("Unit", () => {
             throw new Error();
           }),
         };
-        const DbLocation = makeDbLocation(dependencies);
+        const dbLocation = makeDbLocation(dependencies);
 
         // When
-        const location = DbLocation.add(generateLocationModel());
+        const location = dbLocation.add(generateLocationModel());
 
         // Then
         await expect(location).rejects.toThrow();
@@ -48,13 +55,60 @@ describe("Unit", () => {
 
       it("Should return a location if store succeeds", async () => {
         // Given
-        const DbLocation = makeDbLocation({});
+        const dbLocation = makeDbLocation({});
 
         // When
-        const location = await DbLocation.add(generateLocationModel());
+        const location = await dbLocation.add(generateLocationModel());
 
         // Then
         expect(location).toStrictEqual(generateLocationEntity());
+      });
+    });
+
+    describe("DbLocation.find()", () => {
+      it("Should call LocationRepository with required data", async () => {
+        // GIVEN
+        const dependencies = { find: jest.fn() };
+        const dbLocation = makeDbLocation(dependencies);
+
+        // WHEN
+        await dbLocation.get(locationEntity.brandId, locationEntity.id);
+
+        // THEN
+        expect(dependencies.find).toBeCalledWith(locationEntity.brandId, locationEntity.id);
+      });
+
+      it("Should throw an error if LocationRepository throws", async () => {
+        // Given
+        const dependencies = {
+          find: jest.fn().mockImplementationOnce(() => {
+            throw new Error();
+          }),
+        };
+        const dbLocation = makeDbLocation(dependencies);
+
+        // When
+        const location = dbLocation.get(
+          locationEntity.brandId,
+          locationEntity.id
+        );
+
+        // Then
+        await expect(location).rejects.toThrow();
+      });
+
+      it("Should return the location if found location", async () => {
+        // Given
+        const dbLocation = makeDbLocation({});
+
+        // When
+        const location = await dbLocation.get(
+          locationEntity.brandId,
+          locationEntity.id
+        );
+
+        // Then
+        expect(location).toStrictEqual(locationEntity);
       });
     });
   });
